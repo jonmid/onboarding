98d6d9f8c6bb2bedb1cdca2f555d3d1430555390
Jhonatan Mideros <10jonmid@gmail.com>
2024-10-28 14:54:24 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /* return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const OnboardingScreen(),
-    ); */
-
     return ScreenUtilInit(
       designSize: const Size(390, 844),
       useInheritedMediaQuery: true,
